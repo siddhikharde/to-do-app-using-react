8b22a24ca358cbf61f0b37276dd1101207accf41
siddhikharde <siddhikharde2005@gmail.com>
2025-11-01 10:49:54 +0530

    toaster added

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,59 @@ import React, { useState } from 'react'
 import {Moon, Trash, Plus} from 'lucide-react'
 import "./App.css";
 import TaskCard from './TaskCard.jsx';
+import { toast } from 'react-hot-toast';
 
 function App() {
   const [tasks, setTasks]=useState(["do study","go to the market"]);
   const [newTask , setNewTask]=useState("");
 const date=new Date().toDateString();
 const addTask=()=>{
-  if(newTask.trim()=="")
+  if(newTask.trim()==""){
+    toast.error('Task cannot be empty!');
     return;
-  setTasks([newTask,...tasks ]);
+  } 
+  else{
+ setTasks([newTask,...tasks ]);
   setNewTask("");
+   toast.success('Task added successfully!')
+    }
+ 
+
 }
 const deleteTask=(taskToDelete)=>{
   const updatedTasks=tasks.filter((tasks)=>tasks!==taskToDelete);
   setTasks (updatedTasks);
+  toast.success('Task deleted successfully!')
 }
 
   return (
     <div>
+      
       <div className='container'>
         <div className='headingCon'>
           <h1 className="TaskHeading">My Tasks</h1>
         <p>{date}</p>
+        
         </div>
         <div className='togglerCon'>
           <Moon className='toggler' />
         </div>
       </div>
-      <div className="taskCardCon">
-        {tasks.map((task, index) => (
-          <TaskCard task={task} key={index} deleteTask={()=>{deleteTask(task)}}/>
-        ))}
-      </div>
+      <div className='heading-disc'>The way to get started is to quit talking and begin doing."</div>
+
       <div className='addTaskCon'>
         <input type="text"className='task-input'placeholder='Add a Task' value={newTask} onChange={(e)=>{
             setNewTask(e.target.value);
         }} />
         <button className="add-btn" onClick={addTask} ><Plus/></button>
         </div>
+
+      <div className="taskCardCon">
+        {tasks.map((task, index) => (
+          <TaskCard task={task} key={index} deleteTask={()=>{deleteTask(task)}}/>
+        ))}
+      </div>
+      
     </div>
   )
 }
